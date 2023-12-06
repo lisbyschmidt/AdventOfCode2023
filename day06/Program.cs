319c200807447c7ccc5729ea26e555a491da2041
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2023-12-06 22:20:24 +0100

    day06 in C#

---
+++ day06/Program.cs
@@ -0,0 +1,33 @@
+﻿var paper = File.ReadLines("input.txt").ToArray();
+var races = Environment.GetEnvironmentVariable("part") switch {
+    "part1" => ParseRaces(paper),
+    "part2" => [ParseRace(paper)],
+    _ => throw new InvalidOperationException("Please run with environment variable part=part1 or part=part2")
+};
+Console.WriteLine("C#\n" + races[1..].Aggregate(races[0].WaysToBeatRecord(), (acc, race) => acc * race.WaysToBeatRecord()));
+
+static Race[] ParseRaces(string[] paper) => ParseNumbers(paper[0]).Zip(ParseNumbers(paper[1]),
+    (time, recordDistance) => new Race(time, recordDistance)).ToArray();
+
+static long[] ParseNumbers(string numbers) => numbers[9..].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+
+static Race ParseRace(string[] paper) => new Race(
+    Time: long.Parse(new string(paper[0].Where(char.IsDigit).ToArray())),
+    RecordDistance: long.Parse(new string(paper[1].Where(char.IsDigit).ToArray())));
+
+record Race(long Time, long RecordDistance) {
+    public long WaysToBeatRecord() {
+        var minTimeToHold = TimeRange().First(x => Distance(x) > RecordDistance);
+        var maxTimeToHold = TimeRange().Reverse().First(x => Distance(x) > RecordDistance);
+        return maxTimeToHold - minTimeToHold + 1;
+    }
+
+    private long Distance(long timeHeld) => (Time - timeHeld) * timeHeld;
+
+    private IEnumerable<long> TimeRange(long start = 1) {
+        while (start < Time) {
+            yield return start;
+            start++;
+        }
+    }
+}
