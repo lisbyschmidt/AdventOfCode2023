8dbddd35a17d5c1e84101db3805fdaf0e9f65be4
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2023-12-06 22:53:28 +0100

    Avoiding TimeRange().Reverse() in day06

--- day06/Program.cs
+++ day06/Program.cs
@@ -6,10 +6,12 @@ var races = Environment.GetEnvironmentVariable("part") switch {
 };
 Console.WriteLine("C#\n" + races[1..].Aggregate(races[0].WaysToBeatRecord(), (acc, race) => acc * race.WaysToBeatRecord()));
 
-static Race[] ParseRaces(string[] paper) => ParseNumbers(paper[0]).Zip(ParseNumbers(paper[1]),
-    (time, recordDistance) => new Race(time, recordDistance)).ToArray();
+static Race[] ParseRaces(string[] paper) =>
+    ParseNumbers(paper[0]).Zip(ParseNumbers(paper[1]),
+        (time, recordDistance) => new Race(time, recordDistance)).ToArray();
 
-static long[] ParseNumbers(string numbers) => numbers[9..].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+static long[] ParseNumbers(string numbers) =>
+    numbers[9..].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
 
 static Race ParseRace(string[] paper) => new Race(
     Time: long.Parse(new string(paper[0].Where(char.IsDigit).ToArray())),
@@ -17,17 +19,13 @@ static Race ParseRace(string[] paper) => new Race(
 
 record Race(long Time, long RecordDistance) {
     public long WaysToBeatRecord() {
-        var minTimeToHold = TimeRange().First(x => Distance(x) > RecordDistance);
-        var maxTimeToHold = TimeRange().Reverse().First(x => Distance(x) > RecordDistance);
+        var minTimeToHold = RangeAscending(1, Time - 1).First(x => Distance(x) > RecordDistance);
+        var maxTimeToHold = RangeDescending(Time - 1, 1).First(x => Distance(x) > RecordDistance);
         return maxTimeToHold - minTimeToHold + 1;
     }
 
     private long Distance(long timeHeld) => (Time - timeHeld) * timeHeld;
 
-    private IEnumerable<long> TimeRange(long start = 1) {
-        while (start < Time) {
-            yield return start;
-            start++;
-        }
-    }
+    private static IEnumerable<long> RangeAscending(long start, long end) { while (start <= end) { yield return start; start++; } }
+    private static IEnumerable<long> RangeDescending(long start, long end) { while (start >= end) { yield return start; start--; } }
 }
