49955f8b1ef2303c241ffd6d3ad9a790f9abdbe8
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2023-12-13 22:04:16 +0100

    day09 in C# finally ready to be pushed :)

---
+++ day09/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = File.ReadLines("input.txt").Select(x => x.Split().Select(long.Parse).ToArray()).ToArray();
+Console.WriteLine("C#\n" + Environment.GetEnvironmentVariable("part") switch {
+    "part1" => input.Select(x => ExtrapolateValue(x, seq => seq[^1], (acc, seqVal) => seqVal + acc)).Sum(),
+    "part2" => input.Select(x => ExtrapolateValue(x, seq => seq[0], (acc, seqVal) => seqVal - acc)).Sum(),
+    _ => "Please run with environment variable part=part1 or part=part2"
+});
+
+static long ExtrapolateValue(long[] sequence, Func<long[], long> selector, Func<long, long, long> accumulator) {
+    var endOfSequenceValues = new Stack<long>(new[] { selector(sequence) });
+    while (sequence.Any(x => x != 0)) {
+        sequence = Enumerable.Range(0, sequence.Length - 1).Select(i => sequence[i + 1] - sequence[i]).ToArray();
+        endOfSequenceValues.Push(selector(sequence));
+    }
+    return endOfSequenceValues.Aggregate(accumulator);
+}
